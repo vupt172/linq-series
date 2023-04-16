ebbbc8b96773e5b44e8876387164cdff6a538881
vu.phamtuan <vu.phamtuan@ops.nccsoft.vn>
2023-04-16 17:51:03 +0700

    linq-series-6

---
+++ Linq Series 6/Linq Series 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Series_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            test_1();
+        }
+        private static void test_1()
+        {
+            var numbers = new List<int> { 5, 3, 7, 1, 2, 4 };
+            var numbersWith10 = numbers.Append(10);
+            Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+            Console.WriteLine("Numbers with 10: " + string.Join(", ", numbersWith10));
+
+            //var oddNumbers = numbers.Where(number => number % 2 == 1);
+            // var orderedOddNumbers = oddNumbers.OrderBy(number => number);
+            var orderedOddNumbers = numbers
+                .Where(number => number % 2 == 1)
+                .OrderBy(number => numbers);
+            Console.WriteLine("OrderedOddNumbers: " + string.Join(", ", orderedOddNumbers));
+        }
+    }
+}
