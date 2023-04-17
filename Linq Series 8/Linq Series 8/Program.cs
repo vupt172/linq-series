f699d0f609c6d3b51951dcff353d4f7fe4f19425
vu.phamtuan <vu.phamtuan@ops.nccsoft.vn>
2023-04-17 18:11:07 +0700

    linq-series-8

---
+++ Linq Series 8/Linq Series 8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Series_8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<string>()
+            {
+                "Duck","Lion","Dolphin","Tiger"
+            };
+            var animalsWithD = animals.Where(
+                animal =>
+                {
+                    Console.WriteLine("Check animal: " + animal);
+                    return animal.StartsWith('D'); 
+                }
+                );
+            foreach(var animal in animalsWithD)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
